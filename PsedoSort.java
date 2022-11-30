8fbc905a5729813e28b6dca53b41cb75df444ba5
andyIrish <114688020+andyIrish@users.noreply.github.com>
2022-11-30 15:29:14 -0800

    Create PsedoSort.java
    shaky logic for search and sort 
    im unsure how to sort/search via attributes.
    can i assign values to the recipes as a searchable sortable variable ? ie. chickenFriedSteak = "45";

---
+++ PsedoSort.java
@@ -0,0 +1,6 @@
+// hello team
+
+// while (int i = 0, prepTime.attribute <= 45, i++)
+  //    
+    // sort and determine which indices to print out
+  // System.out.println("here are recipes to make im unda 45 minutes you salty little minx %s ", + recipes[i] ); // keep this in the while loop
